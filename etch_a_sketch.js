e26299d16537eecf0a74c43a3bbf093001a2db5e
DashikiBulbasaur <githubearl@gmail.com>
2026-08-26 23:40:51 -0600

    finish grid of 16 boxes

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,18 +1,12 @@
 const container = document.createElement("div");
+container.classList.add("div_container");
 
-const div_1 = document.createElement("div");
-const div_2 = document.createElement("div");
-const div_3 = document.createElement("div");
-const div_4 = document.createElement("div");
-const div_5 = document.createElement("div");
-const div_6 = document.createElement("div");
-const div_7 = document.createElement("div");
-const div_8 = document.createElement("div");
-const div_9 = document.createElement("div");
-const div_10 = document.createElement("div");
-const div_11 = document.createElement("div");
-const div_12 = document.createElement("div");
-const div_13 = document.createElement("div");
-const div_14 = document.createElement("div");
-const div_15 = document.createElement("div");
-const div_16 = document.createElement("div");
+const list_of_boxes = []
+for (i = 0; i < 16; i++) {
+    const div = document.createElement("div");
+    list_of_boxes.push(div);
+    container.appendChild(div);
+}
+list_of_boxes.forEach(element => element.classList.add("box"));
+
+document.body.appendChild(container);
