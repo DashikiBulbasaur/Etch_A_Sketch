6c00b62bcd5861b0e78d50c071fdea56858bc401
DashikiBulbasaur <githubearl@gmail.com>
2026-08-31 19:00:00 -0600

    finish pixelated trail feature

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -2,8 +2,25 @@ const container = document.createElement("div");
 container.classList.add("div_container");
 
 const list_of_boxes = []
+const colors = ["#FF007F", "#00F0FF", "#FFD700", "#7000FF", "#39FF14"];
 for (i = 0; i < 256; i++) {
     const div = document.createElement("div");
+    div.addEventListener("mousemove", (event) => {
+        const dot = document.createElement("div");
+        dot.className = "trail";
+
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        dot.style.backgroundColor = randomColor;
+
+        dot.style.left = `${event.clientX}px`;
+        dot.style.top = `${event.clientY}px`;
+
+        document.body.appendChild(dot);
+
+        setTimeout(() => {
+            dot.remove();
+        }, 800);
+    });
     list_of_boxes.push(div);
     container.appendChild(div);
 }
